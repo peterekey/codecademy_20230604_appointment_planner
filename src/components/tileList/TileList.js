a1711195f7fb84e71f811419d200b158f9928028
Peter Key <63628694+peterekey@users.noreply.github.com>
2023-06-13 09:08:17 +1000

    Step 5: Create TileList component

--- src/components/tileList/TileList.js
+++ src/components/tileList/TileList.js
@@ -1,9 +1,26 @@
 import React from "react";
+import { Tile } from '../tile/Tile'
 
-export const TileList = (contacts) => {
+export const TileList = ({contacts}) => {
+
+  const cards = []
+
+  contacts.map((contact, index) => {
+
+    const { contactName: name, ...rest } = contact
+    
+    return cards.push(
+      <Tile 
+        name={name}
+        description={rest}
+        key={index}
+      />
+    )
+  })
 
   return (
     <div>
+      {cards}
     </div>
   );
 };
