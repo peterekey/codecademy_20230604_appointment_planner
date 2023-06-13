5fb6a1f6e7384bf2186a63785f38b091a0873acd
Peter Key <63628694+peterekey@users.noreply.github.com>
2023-06-13 22:36:35 +1000

    Step 7: Set up AppointmentsPage component

--- src/containers/appointmentsPage/AppointmentsPage.js
+++ src/containers/appointmentsPage/AppointmentsPage.js
@@ -3,11 +3,15 @@ import React, { useState } from "react";
 import { AppointmentForm } from "../../components/appointmentForm/AppointmentForm";
 import { TileList } from "../../components/tileList/TileList";
 
-export const AppointmentsPage = () => {
+export const AppointmentsPage = ({appointments, addAppointment, contacts}) => {
   /*
   Define state variables for 
   appointment info
   */
+ const [currentName, setCurrentName] = useState('')
+ const [currentContact, setCurrentContact] = useState('')
+ const [currentDate, setCurrentDate] = useState('')
+ const [currentTime, setCurrentTime] = useState('') 
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -21,10 +25,22 @@ export const AppointmentsPage = () => {
     <div>
       <section>
         <h2>Add Appointment</h2>
+        <AppointmentForm 
+          name={currentName}
+          setName={setCurrentName}
+          contact={currentContact}
+          setContact={setCurrentContact}
+          date={currentDate}
+          setDate={setCurrentDate}
+          time={currentTime}
+          setTime={setCurrentTime}
+          handleSubmit={handleSubmit}
+        />
       </section>
       <hr />
       <section>
         <h2>Appointments</h2>
+        <TileList items={appointments}/>
       </section>
     </div>
   );
