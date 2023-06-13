9c639acf89920379d1442248e333309a70f1a69b
Peter Key <63628694+peterekey@users.noreply.github.com>
2023-06-13 21:53:16 +1000

    Step 6: Set up Tile component

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -1,9 +1,13 @@
 import React from "react";
 
-export const Tile = () => {
+export const Tile = ({name, description}) => {
+
   return (
     <div className="tile-container">
-      
+      <p className="tile-title">{name}</p>
+      {  Object.values(description).map((item, index) => {
+        return <p key={index} className="tile">{item}</p>
+      }) }
     </div>
   );
 };
